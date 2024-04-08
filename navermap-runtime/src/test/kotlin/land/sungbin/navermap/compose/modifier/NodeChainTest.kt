af309a4f951d025a4f4c551ccd55373ca45ea6b6
jisungbin <ji@sungb.in>
2024-04-08 12:05:22 +0900

    Spotless make happy

--- navermap-runtime/src/test/kotlin/land/sungbin/navermap/compose/modifier/NodeChainTest.kt
+++ navermap-runtime/src/test/kotlin/land/sungbin/navermap/compose/modifier/NodeChainTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 SOUP, Ji Sungbin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package land.sungbin.navermap.compose.modifier
 
 import assertk.assertThat
@@ -10,16 +26,14 @@ import land.sungbin.navermap.runtime.modifier.MapModifierNode
 import land.sungbin.navermap.runtime.modifier.dirtyForModifiers
 
 class NodeChainTest {
-  @Test
-  fun dirtyForModifiersReuse() {
+  @Test fun dirtyForModifiersReuse() {
     val node = NothingMapModifierNode()
 
     val dirty = dirtyForModifiers(node, node)
     assertThat(dirty).isEqualTo(ActionReuse)
   }
 
-  @Test
-  fun dirtyForModifiersUpdate() {
+  @Test fun dirtyForModifiersUpdate() {
     val node = NothingMapModifierNode()
     val node2 = NothingMapModifierNode()
 
@@ -27,8 +41,7 @@ class NodeChainTest {
     assertThat(dirty).isEqualTo(ActionUpdate)
   }
 
-  @Test
-  fun dirtyForModifiersReplace() {
+  @Test fun dirtyForModifiersReplace() {
     val node = NothingMapModifierNode()
     val node2 = UnitMapModifierNode()
 
