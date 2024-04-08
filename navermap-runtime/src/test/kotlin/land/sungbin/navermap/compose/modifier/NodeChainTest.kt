4429838ec6f7caf0e87092be203e260a058f543d
jisungbin <ji@sungb.in>
2024-04-08 09:17:19 +0900

    dirtyForModifiers tests

--- navermap-runtime/src/test/kotlin/land/sungbin/navermap/compose/modifier/NodeChainTest.kt
+++ navermap-runtime/src/test/kotlin/land/sungbin/navermap/compose/modifier/NodeChainTest.kt
@@ -1,12 +1,48 @@
 package land.sungbin.navermap.compose.modifier
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import kotlin.test.Test
-import land.sungbin.navermap.runtime.modifier.MapModifierNodeChain
+import land.sungbin.navermap.runtime.modifier.ActionReplace
+import land.sungbin.navermap.runtime.modifier.ActionReuse
+import land.sungbin.navermap.runtime.modifier.ActionUpdate
+import land.sungbin.navermap.runtime.modifier.MapModifierNode
+import land.sungbin.navermap.runtime.modifier.dirtyForModifiers
 
 class NodeChainTest {
   @Test
-  fun testForTest() {
-    val chain = MapModifierNodeChain(emptyList())
-    println(chain)
+  fun dirtyForModifiersReuse() {
+    val node = NothingMapModifierNode()
+
+    val dirty = dirtyForModifiers(node, node)
+    assertThat(dirty).isEqualTo(ActionReuse)
   }
+
+  @Test
+  fun dirtyForModifiersUpdate() {
+    val node = NothingMapModifierNode()
+    val node2 = NothingMapModifierNode()
+
+    val dirty = dirtyForModifiers(node, node2)
+    assertThat(dirty).isEqualTo(ActionUpdate)
+  }
+
+  @Test
+  fun dirtyForModifiersReplace() {
+    val node = NothingMapModifierNode()
+    val node2 = UnitMapModifierNode()
+
+    val dirty = dirtyForModifiers(node, node2)
+    assertThat(dirty).isEqualTo(ActionReplace)
+  }
+}
+
+class NothingMapModifierNode : MapModifierNode<Nothing> {
+  override fun hashCode(): Int = System.identityHashCode(this)
+  override fun equals(other: Any?) = this.hashCode() == other.hashCode()
+}
+
+class UnitMapModifierNode : MapModifierNode<Unit> {
+  override fun hashCode(): Int = System.identityHashCode(this)
+  override fun equals(other: Any?) = this.hashCode() == other.hashCode()
 }
