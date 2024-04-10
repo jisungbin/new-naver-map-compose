480965c3ab679404782c5da172d499d9579951ca
jisungbin <ji@sungb.in>
2024-04-10 23:08:18 +0900

    Sample!

--- sample/src/main/kotlin/land/sungbin/navermap/sample/SampleActivity.kt
+++ sample/src/main/kotlin/land/sungbin/navermap/sample/SampleActivity.kt
@@ -19,15 +19,42 @@ package land.sungbin.navermap.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import land.sungbin.navermap.ui.NaverMap
 
 class SampleActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      NaverMap(modifier = Modifier.fillMaxSize())
+      Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(30.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+      ) {
+        var showMap by remember { mutableStateOf(false) }
+
+        AnimatedVisibility(
+          modifier = Modifier.fillMaxSize(0.5f),
+          visible = showMap,
+        ) {
+          NaverMap(modifier = Modifier.fillMaxSize())
+        }
+        Button(onClick = { showMap = !showMap }) {
+          Text(text = if (showMap) "Hide Map" else "Show Map")
+        }
+      }
     }
   }
 }
